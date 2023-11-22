9ddaf9fa2a4fa9bcdf3f755ac785e496dd1139c0
Haripriyabellamkonda <haripriyabellamkonda05@gmail.com>
2023-11-22 17:08:06 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ db.on("error",() => console.log("error"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors(
+    {
+        origin :"*",
+        methods:['GET','HEAD','PUT','PATCH','POST','DELETE'],
+        credentials: true,
+    }
+));
 
 
 app.use("/",route);
